0879026b7df6458d21c113905bc034b07d5ada66
Tiago Ribeiro Pereira <tiagorptb24@gmail.com>
2026-08-11 15:42:53 -0300

    build: Constructing the project

---
+++ apps/web/eslint.config.js
@@ -0,0 +1,28 @@
+import js from "@eslint/js";
+import globals from "globals";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import jsxA11y from "eslint-plugin-jsx-a11y";
+import tseslint from "typescript-eslint";
+import { defineConfig, globalIgnores } from "eslint/config";
+
+export default defineConfig([
+	globalIgnores(["dist"]),
+	{
+		files: ["**/*.{ts,tsx}"],
+		extends: [
+			js.configs.recommended,
+			tseslint.configs.recommendedTypeChecked,
+			reactHooks.configs.flat.recommended,
+			reactRefresh.configs.vite,
+			jsxA11y.flatConfigs.recommended,
+		],
+		languageOptions: {
+			globals: globals.browser,
+			parserOptions: {
+				project: "./tsconfig.json",
+				tsconfigRootDir: import.meta.dirname,
+			},
+		},
+	},
+]);
